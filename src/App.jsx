1238e804f06f1c0159623d4bc950fab0dfb223ee
kely2822 <ferlysialily@gmail.com>
2026-04-13 23:22:56 +0700

    add wishlist page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginPage from './login/index.jsx'
 import TwoFAPage from './2fa/index.jsx'
 import RegisterPage from './register/register.jsx'
+import WishlistPage from './wishlist/wishlist.jsx'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/2fa" element={<TwoFAPage />} />
+        <Route path="/wishlist" element={<WishlistPage />} />
       </Routes>
     </Router>
   )
