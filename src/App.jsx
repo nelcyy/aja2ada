412ccd06d05148432a2ca34cdcf2eab902731364
nelcyy <lodarmasenelcy@gmail.com>
2026-04-13 21:32:02 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginPage from './login/index.jsx'
-<<<<<<< HEAD
-import RegisterPage from './register/index.jsx'
 import TwoFAPage from './2fa/index.jsx'
-=======
 import RegisterPage from './register/register.jsx'
->>>>>>> da882488ff42278776b85bb7fb46d623ba0eb63d
 
 function App() {
   return (
