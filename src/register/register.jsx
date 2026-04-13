9d817a588a3954b7492598d61bb8362d490c0da1
fidelia tanner <fideliatanner@gmail.com>
2026-04-13 22:22:29 +0700

    fixed register page style

--- src/register/register.jsx
+++ src/register/register.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import "./register.css";
+import "./Register.css";
 
 export default function Register() {
   return (
-    <div className="container">
-      <div className="card">
+    <div className="page">
+      <div className="wrapper">
         <img src="/logo-careofyou.png" alt="logo" className="logo" />
 
-        <input type="text" placeholder="Name" className="input" />
-        <input type="email" placeholder="Email" className="input" />
-        <input type="password" placeholder="Password" className="input" />
-        <input type="password" placeholder="Confirm Password" className="input" />
+        <div className="form">
+          <input type="text" placeholder="Name" className="input" />
+          <input type="email" placeholder="Email" className="input" />
+          <input type="password" placeholder="Password" className="input" />
+          <input type="password" placeholder="Confirm Password" className="input" />
 
-        <button className="btn">Register</button>
+          <button className="btn">Register</button>
+        </div>
 
         <p className="back">Back to Login</p>
       </div>
